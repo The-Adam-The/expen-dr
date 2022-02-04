bd408d4a7a37dc46d90e527801b79fda3362fdf0
codeclan student <codeclanstudent@codeclans-mbp.home>
2022-02-04 15:59:28 +0000

    Added and tested select_all() for merchant_repo

--- console.py
+++ console.py
@@ -7,3 +7,8 @@ import repositories.merchant_repository as merchant_repository
 
 merchant1 = Merchant('Tesco')
 merchant_repository.save(merchant1)
+
+results = merchant_repository.select_all()
+
+for result in results:
+    print(result.__dict__)
--- repositories/merchant_repository.py
+++ repositories/merchant_repository.py
@@ -1,5 +1,6 @@
 from db.run_sql import run_sql
 
+from models.merchant import Merchant
 
 def save(merchant):
     sql = "INSERT INTO merchants(name) VALUES (%s) RETURNING id"
@@ -7,4 +8,17 @@ def save(merchant):
     results = run_sql(sql, values)
     merchant.id = results[0]['id']
     return merchant
-    
+
+def select_all():
+
+    merchants = []
+
+    sql = "SELECT * FROM merchants"
+    results = run_sql(sql)
+
+    if results is not None:
+        for row in results:
+            merchant = Merchant(row['name'], row['id'])
+            merchants.append(merchant)
+
+    return merchants
