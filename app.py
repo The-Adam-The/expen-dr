19f11e6e2f8feeea74d465ab900094fda1e3dc96
codeclan student <codeclanstudent@codeclans-mbp.home>
2022-02-05 13:15:08 +0000

    Created and tested merchant_controller, add html pages for merchant_controller, created base.html and added nav bar

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
 
+from controllers.merchant_controller import merchants_blueprint
+
 
 app = Flask(__name__)
+
+app.register_blueprint(merchants_blueprint)
 
 
 @app.route('/')
--- controllers/merchant_controller.py
+++ controllers/merchant_controller.py
@@ -1,4 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import Blueprint
 
+import repositories.merchant_repository as merchant_repository
 
+merchants_blueprint = Blueprint("merchants", __name__)
+
+@merchants_blueprint.route("/merchants")
+def merchants():
+    merchants = merchant_repository.select_all()
+    return render_template("merchants/index.html", merchants=merchants)
